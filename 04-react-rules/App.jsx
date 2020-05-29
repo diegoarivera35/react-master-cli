c934de6426c75721796cfc6c9996231c559c91cd
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-05-28 23:26:55 -0500

    Revert "feat(04): add errors for JSX"
    This reverts commit 8539355e2ef9a9556a5d7632b43b9dfee64796c7.

--- 04-react-rules/App.jsx
+++ 04-react-rules/App.jsx
@@ -5,23 +5,25 @@ const App = () => {
     const date = new Date().getFullYear();
 
     return (
+        <>
             <main>
                 <figure>
                     <h2>HTML</h2>
-                    <img src="../public/doge.png" alt="doge" >
+                    <img src="/doge.png" alt="doge" />
                     <figcaption>
                         Hay 345 errores pero voy a renderizar lo que entiendo
                     </figcaption>
                 </figure>
                 <figure>
                     <h2>JSX</h2>
-                    <img src="../public/cheems.png" alt="cheems" class="small" >
+                    <img src="/cheems.png" alt="cheems" className="small" />
                     <figcaption>
                         Mamá, falta un slash y no muestro nada
                     </figcaption>
                 </figure>
             </main>
-            <footer>deBakatas memes date ©</footer>
+            <footer>deBakatas memes {date} ©</footer>
+        </>
     );
 };
 
